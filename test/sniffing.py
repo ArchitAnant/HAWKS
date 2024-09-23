099dfde0a2bc8acf81d10c5ec60bb29503b567b2
ArchitAnant <architanant5@gmail.com>
2024-09-23 21:42:08 +0530

    Dividing packet sniffing to periods

--- test/sniffing.py
+++ test/sniffing.py
@@ -1,7 +1,14 @@
 from scapy.all import sniff
 from datetime import datetime
+import statistics as st
 
 # Function to process each packet
+dest_ips = set()
+scr_ips = set()
+time_list = []
+size_list = []
+
+protocol_set = set()
 
 def process_packet(packet):
     """
@@ -14,9 +21,36 @@ def process_packet(packet):
         dst_ip = packet['IP'].dst
         protocol = packet['IP'].proto
         packet_size = len(packet)
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        timestamp = packet.time#datetime.now().second()
 
-        print(f'Time: {timestamp}, Source IP: {src_ip}, Destination IP: {dst_ip}, Size: {packet_size} bytes, Protocol: {protocol}')
+        scr_ips.add(src_ip)
+        dest_ips.add(dst_ip)
+        protocol_set.add(protocol)
+        time_list.append(timestamp)
+        size_list.append(packet_size)
+
+        # print(f'Time: {timestamp}, Source IP: {src_ip}, Destination IP: {dst_ip}, Size: {packet_size} bytes, Protocol: {protocol}')
 
 # Start sniffing the network traffic
-sniff(prn=process_packet, store=0)
+while True:
+    sniff(timeout =5,prn=process_packet, store=0)
+    print("Done with one")
+    # need to handle when there are 0 or 1 items for variance calculation 
+    time_variance = st.variance(time_list)
+    pakt_size = [st.mode(size_list),st.variance(size_list)]
+    print(f"dest ip : {dest_ips}\nsrc ips : {scr_ips}\ntimes : {time_variance}\nSizes : {pakt_size}")
+    dest_ips.clear()
+    scr_ips.clear()
+    time_list.clear()
+    size_list.clear()
+
+
+
+"""
+every dump_pack:
+    1. time variance
+    2. source ip
+    3. dest ip
+    4. size(mode,variance)
+    5. protocol
+"""
